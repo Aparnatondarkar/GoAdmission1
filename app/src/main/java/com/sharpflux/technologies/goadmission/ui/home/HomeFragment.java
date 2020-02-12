0e99ff646b370a0775203d97eabe24ab6cc3e367
SharpFlux 1 <aparna.sharpflux@gmail.com>
2020-02-12 10:42:17 +0530

    Initial commit

---
+++ app/src/main/java/com/sharpflux/technologies/goadmission/ui/home/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.sharpflux.technologies.goadmission.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.sharpflux.technologies.goadmission.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        //toolbar_home = (Toolbar)root.findViewById(R.id.toolbar_home);
+        return root;
+    }
+}
